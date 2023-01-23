fea1d99a74992ea93dd1414abe1afc872459da23
Medetcan Diler <118726723+medetcandiler@users.noreply.github.com>
2023-01-23 20:52:41 +0300

    to do list all JavaScript codes were changed by meddo. I used loops, callback functions to create more UX friendly and now when remove to do list element it also remove from localstorage

--- main.js
+++ main.js
@@ -5,58 +5,122 @@ const ALERT=(className='danger', title, message)=>
 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 `
-// const TOAST=(message)=>
-// `
-// <div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
-//   <div class="d-flex">
-//     <div class="toast-body">
-//       Hello, world! This is a toast message.
-//     </div>
-//     <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-//   </div>
-// </div>
-// `
-const ulDOM=document.querySelector('#list');
+
+const itemAreaDOM=document.querySelector('#list');
 const inputDOM=document.querySelector("#input");
 const formDOM=document.querySelector('#form');
 const BUTTON=document.querySelector('#button');
-const myDataArr=localStorage.getItem('items')? JSON.parse(localStorage.getItem('items')) : [];
 const alertDOM=document.querySelector('#alert')
+const dataArray=localStorage.getItem('myDataItems')? JSON.parse(localStorage.getItem('myDataItems')) : [];
 
-//checked class toggle to the every list item 
-ulDOM.addEventListener('click', function(e) {
-  if (e.target.tagName == 'LI') {
-    e.target.classList.toggle('checked');
-  }
-}, false);
+itemAreaDOM.addEventListener('click', (e) => {
+    if(e.target.tagName == 'NAV'){
+        e.target.classList.toggle('checked')
+    }
+})
 
-//submit event with delete button and remove list item from list in one function
-formDOM.addEventListener('submit', function(e){
-    e.preventDefault();
-    if(inputDOM.value.length<5){
-        alertDOM.innerHTML=ALERT('danger', 'Wrong Attempt', 'You need write something at least 5 character');
-        setTimeout(()=>{
+formDOM.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let item=document.querySelector('input');
+    createList(item);
+})
+
+function createList(item){
+    if(item.value.length<5){
+        alertDOM.innerHTML=ALERT('danger', 'Wrong Attempt', 'Write at least 5 character');
+        setTimeout(()=> {
             alertDOM.innerHTML=''
         },1500)
     }else {
-        let newLi=document.createElement('li');
-        newLi.innerHTML=inputDOM.value;
-        ulDOM.appendChild(newLi)
-        myDataArr.push(inputDOM.value);
-        alertDOM.innerHTML=ALERT('success', 'Conguratilations', 'Your request succesfully added to the list')
-        localStorage.setItem('items', JSON.stringify(myDataArr));
-        setTimeout(()=>alertDOM.innerHTML='',1500)
-        inputDOM.value=''
-        let deleteButton=document.createElement('span');
-        let text=document.createTextNode('\u2715');
-        deleteButton.appendChild(text);
-        newLi.appendChild(deleteButton);
-        newLi.classList.add('rounded');
-        deleteButton.addEventListener('click', function(e){
-            let listItem=this.parentNode;
-            let ul=listItem.parentNode;
-            ul.removeChild(listItem)
-        })
+        dataArray.push(item.value)
+        localStorage.setItem('myDataItems', JSON.stringify(dataArray))
+        console.log(dataArray)
+        location.reload()
     }
-})
+}
+
+function displayItems(){
+    let items=''
+    for(let i of dataArray){
+        items+=`
+            <nav class="list-group-item d-flex justify-content-between " id='span'><span >${i}</span> 
+                <i class="fa-solid fa-check deleteBtn"></i>
+            </nav>
+        `
+    }document.querySelector('#list').innerHTML=items
+    deleteItem()
+}
+
+function deleteItem(){
+    let deleteBtn=document.querySelectorAll('.deleteBtn')
+    deleteBtn.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            deleteListItem(i)
+        })
+    })
+}
+function deleteListItem(i){
+    dataArray.splice(i,1);
+    localStorage.setItem('myDataItems' , JSON.stringify(dataArray))
+    location.reload()
+}
+
+window.onload = function(){
+    displayItems()
+}
+
+
+//         let deleteButton=document.createElement('span');
+//         let text=document.createTextNode('\u2715');
+//         deleteButton.appendChild(text);
+//         newLi.appendChild(deleteButton);
+//         newLi.classList.add('rounded');
+//         deleteButton.addEventListener('click', function(e){
+//             let listItem=this.parentNode;
+//             let ul=listItem.parentNode;
+//             ul.removeChild(listItem)
+//         })
+
+
+//checked class toggle to the every list item 
+// ulDOM.addEventListener('click', (e) => {
+//     if(e.target.tagName== 'LI' ){
+//         e.target.classList.toggle('checked')
+//     }
+// })
+
+//submit event with delete button and remove list item from list in one function
+// formDOM.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     if(inputDOM.value.length<5){
+//         alertDOM.innerHTML=ALERT('danger', 'Wrong Attempt', 'You need write something at least 5 character');
+//         setTimeout(()=>{
+//             alertDOM.innerHTML=''
+//         },1500)
+//     }else {
+//         let newLi=document.createElement('li');
+//         newLi.innerHTML=inputDOM.value;
+//         ulDOM.appendChild(newLi)
+//         myDataArr.push(inputDOM.value);
+//         alertDOM.innerHTML=ALERT('success', 'Conguratilations', 'Your request succesfully added to the list')
+//         localStorage.setItem('items', JSON.stringify(myDataArr));
+//         setTimeout(()=>alertDOM.innerHTML='',1500)
+//         inputDOM.value=''
+//         deleteItem()
+//     }
+// })
+
+// function deleteItem(){
+//     let deleteButton=document.createElement('span');
+//     let text=document.createTextNode('\u2715');
+//     deleteButton.appendChild(text);
+//     newLi.appendChild(deleteButton);
+//     newLi.classList.add('rounded');
+//     deleteButton.addEventListener('click', function(e){
+//         let listItem=this.parentNode;
+//         let ul=listItem.parentNode;
+//         ul.removeChild(listItem)
+//     })
+// }
+
 
